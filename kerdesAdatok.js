0aba7e487126aefea9284f9516b39620580384af
Frnc36 <magoriferenc19@gmail.com>
2026-05-14 13:31:23 +0200

    adat fajl hozzadva

---
+++ kerdesAdatok.js
@@ -0,0 +1,40 @@
+const kvizAdatok = [
+  {
+    nev: "JavaScript",
+    kerdesek: [
+      {
+        kerdesSzoveg: "Mit jelent a DOM?",
+        valaszok: [
+          { szoveg: "Document Object Model", helyes: true },
+          { szoveg: "Data Object Method", helyes: false },
+          { szoveg: "Display Object Manager", helyes: false },
+          { szoveg: "Digital Output Mode", helyes: false },
+        ],
+      },
+      {
+        kerdesSzoveg:
+          "Melyik kulcsszóval lehet változót létrehozni JavaScriptben?",
+        valaszok: [
+          { szoveg: "let", helyes: true },
+          { szoveg: "make", helyes: false },
+          { szoveg: "newvar", helyes: false },
+          { szoveg: "variable", helyes: false },
+        ],
+      },
+    ],
+  },
+  {
+    nev: "PHP",
+    kerdesek: [
+      {
+        kerdesSzoveg: "Mivel kezdődik egy PHP változó neve?",
+        valaszok: [
+          { szoveg: "$ jellel", helyes: true },
+          { szoveg: "# jellel", helyes: false },
+          { szoveg: "@ jellel", helyes: false },
+          { szoveg: "& jellel", helyes: false },
+        ],
+      },
+    ],
+  },
+];
