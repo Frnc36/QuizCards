d30540197d50a5559dc9b055c4dc4d83224bbd3b
molnarbea <molnarbeatrix11@gmail.com>
2026-05-18 20:43:22 +0200

    kerdesAdatok kibovitve 10-10 kerdessel

--- kerdesAdatok.js
+++ kerdesAdatok.js
@@ -12,27 +12,276 @@ export const kvizAdatok = [
         ],
       },
       {
-        kerdesSzoveg:
-          "Melyik kulcsszóval lehet változót létrehozni JavaScriptben?",
+        kerdesSzoveg: "Melyik kulcsszóval deklarálunk változót?",
         valaszok: [
           { szoveg: "let", helyes: true },
           { szoveg: "make", helyes: false },
-          { szoveg: "newvar", helyes: false },
-          { szoveg: "variable", helyes: false },
+          { szoveg: "varible", helyes: false },
+          { szoveg: "new", helyes: false },
+        ],
+      },
+      {
+        kerdesSzoveg: "Melyik adattípus?",
+        valaszok: [
+          { szoveg: "string", helyes: true },
+          { szoveg: "letter", helyes: false },
+          { szoveg: "word", helyes: false },
+          { szoveg: "sentence", helyes: false },
+        ],
+      },
+      {
+        kerdesSzoveg: "Mi a console.log() célja?",
+        valaszok: [
+          { szoveg: "Kiírás konzolra", helyes: true },
+          { szoveg: "Adat mentés", helyes: false },
+          { szoveg: "HTML generálás", helyes: false },
+          { szoveg: "CSS módosítás", helyes: false },
+        ],
+      },
+      {
+        kerdesSzoveg: "Mi a typeof operátor?",
+        valaszok: [
+          { szoveg: "Típus lekérdezés", helyes: true },
+          { szoveg: "Összeadás", helyes: false },
+          { szoveg: "Eltárolás", helyes: false },
+          { szoveg: "Összehasonlítás", helyes: false },
+        ],
+      },
+      {
+        kerdesSzoveg: "Mi az array?",
+        valaszok: [
+          { szoveg: "Tömb", helyes: true },
+          { szoveg: "Sztring", helyes: false },
+          { szoveg: "Szám", helyes: false },
+          { szoveg: "Objektum kulcs", helyes: false },
+        ],
+      },
+      {
+        kerdesSzoveg: "Melyik ciklus létezik?",
+        valaszok: [
+          { szoveg: "for", helyes: true },
+          { szoveg: "repeat", helyes: false },
+          { szoveg: "loopit", helyes: false },
+          { szoveg: "cycle", helyes: false },
+        ],
+      },
+      {
+        kerdesSzoveg: "Mi a === operátor?",
+        valaszok: [
+          { szoveg: "Szigorú egyenlőség", helyes: true },
+          { szoveg: "Összeadás", helyes: false },
+          { szoveg: "Kivonás", helyes: false },
+          { szoveg: "String összefűzés", helyes: false },
+        ],
+      },
+      {
+        kerdesSzoveg: "Mi az event?",
+        valaszok: [
+          { szoveg: "Esemény", helyes: true },
+          { szoveg: "Változó", helyes: false },
+          { szoveg: "Fájl", helyes: false },
+          { szoveg: "Tömb", helyes: false },
+        ],
+      },
+      {
+        kerdesSzoveg: "Mi a function?",
+        valaszok: [
+          { szoveg: "Függvény", helyes: true },
+          { szoveg: "Objektum", helyes: false },
+          { szoveg: "Tömb", helyes: false },
+          { szoveg: "CSS elem", helyes: false },
         ],
       },
     ],
   },
+
   {
     nev: "PHP",
     kerdesek: [
       {
-        kerdesSzoveg: "Mivel kezdődik egy PHP változó neve?",
+        kerdesSzoveg: "Mivel kezdődik egy változó?",
         valaszok: [
-          { szoveg: "$ jellel", helyes: true },
-          { szoveg: "# jellel", helyes: false },
-          { szoveg: "@ jellel", helyes: false },
-          { szoveg: "& jellel", helyes: false },
+          { szoveg: "$", helyes: true },
+          { szoveg: "#", helyes: false },
+          { szoveg: "@", helyes: false },
+          { szoveg: "&", helyes: false },
+        ],
+      },
+      {
+        kerdesSzoveg: "Mi a echo?",
+        valaszok: [
+          { szoveg: "Kiírás", helyes: true },
+          { szoveg: "Változó", helyes: false },
+          { szoveg: "Ciklus", helyes: false },
+          { szoveg: "Függvény", helyes: false },
+        ],
+      },
+      {
+        kerdesSzoveg: "Mi a PHP?",
+        valaszok: [
+          { szoveg: "Szerveroldali nyelv", helyes: true },
+          { szoveg: "CSS", helyes: false },
+          { szoveg: "Java könyvtár", helyes: false },
+          { szoveg: "HTML elem", helyes: false },
+        ],
+      },
+      {
+        kerdesSzoveg: "Melyik ciklus létezik?",
+        valaszok: [
+          { szoveg: "foreach", helyes: true },
+          { szoveg: "loop", helyes: false },
+          { szoveg: "repeat", helyes: false },
+          { szoveg: "cycle", helyes: false },
+        ],
+      },
+      {
+        kerdesSzoveg: "Mi az array?",
+        valaszok: [
+          { szoveg: "Tömb", helyes: true },
+          { szoveg: "Sztring", helyes: false },
+          { szoveg: "Szám", helyes: false },
+          { szoveg: "Objektum", helyes: false },
+        ],
+      },
+      {
+        kerdesSzoveg: "Mi a $_GET?",
+        valaszok: [
+          { szoveg: "Adat lekérés", helyes: true },
+          { szoveg: "Adat törlés", helyes: false },
+          { szoveg: "Adat mentés", helyes: false },
+          { szoveg: "CSS", helyes: false },
+        ],
+      },
+      {
+        kerdesSzoveg: "Mi a isset()?",
+        valaszok: [
+          { szoveg: "Ellenőrzi létezik-e", helyes: true },
+          { szoveg: "Töröl", helyes: false },
+          { szoveg: "Kiír", helyes: false },
+          { szoveg: "Másol", helyes: false },
+        ],
+      },
+      {
+        kerdesSzoveg: "Mi a include?",
+        valaszok: [
+          { szoveg: "Fájl beillesztés", helyes: true },
+          { szoveg: "Fájl törlés", helyes: false },
+          { szoveg: "Kimenet", helyes: false },
+          { szoveg: "Ciklus", helyes: false },
+        ],
+      },
+      {
+        kerdesSzoveg: "Mi a ==?",
+        valaszok: [
+          { szoveg: "Összehasonlítás", helyes: true },
+          { szoveg: "Összeadás", helyes: false },
+          { szoveg: "Kivonás", helyes: false },
+          { szoveg: "Törlés", helyes: false },
+        ],
+      },
+      {
+        kerdesSzoveg: "Mi a session?",
+        valaszok: [
+          { szoveg: "Session adat", helyes: true },
+          { szoveg: "CSS", helyes: false },
+          { szoveg: "HTML", helyes: false },
+          { szoveg: "Java", helyes: false },
+        ],
+      },
+    ],
+  },
+
+  {
+    nev: "Java",
+    kerdesek: [
+      {
+        kerdesSzoveg: "Mi a Java?",
+        valaszok: [
+          { szoveg: "Programozási nyelv", helyes: true },
+          { szoveg: "Weboldal", helyes: false },
+          { szoveg: "CSS", helyes: false },
+          { szoveg: "HTML", helyes: false },
+        ],
+      },
+      {
+        kerdesSzoveg: "Mi a main()?",
+        valaszok: [
+          { szoveg: "Belépési pont", helyes: true },
+          { szoveg: "Ciklus", helyes: false },
+          { szoveg: "Változó", helyes: false },
+          { szoveg: "Objektum", helyes: false },
+        ],
+      },
+      {
+        kerdesSzoveg: "Mi a class?",
+        valaszok: [
+          { szoveg: "Osztály", helyes: true },
+          { szoveg: "Függvény", helyes: false },
+          { szoveg: "Sztring", helyes: false },
+          { szoveg: "Tömb", helyes: false },
+        ],
+      },
+      {
+        kerdesSzoveg: "Mi az objektum?",
+        valaszok: [
+          { szoveg: "Osztály példánya", helyes: true },
+          { szoveg: "Tömb", helyes: false },
+          { szoveg: "String", helyes: false },
+          { szoveg: "Ciklus", helyes: false },
+        ],
+      },
+      {
+        kerdesSzoveg: "Mi a String?",
+        valaszok: [
+          { szoveg: "Szöveg", helyes: true },
+          { szoveg: "Szám", helyes: false },
+          { szoveg: "Objektum", helyes: false },
+          { szoveg: "Függvény", helyes: false },
+        ],
+      },
+      {
+        kerdesSzoveg: "Mi a int?",
+        valaszok: [
+          { szoveg: "Egész szám", helyes: true },
+          { szoveg: "Sztring", helyes: false },
+          { szoveg: "Logikai", helyes: false },
+          { szoveg: "Objektum", helyes: false },
+        ],
+      },
+      {
+        kerdesSzoveg: "Mi a for?",
+        valaszok: [
+          { szoveg: "Ciklus", helyes: true },
+          { szoveg: "Függvény", helyes: false },
+          { szoveg: "Osztály", helyes: false },
+          { szoveg: "Típus", helyes: false },
+        ],
+      },
+      {
+        kerdesSzoveg: "Mi a boolean?",
+        valaszok: [
+          { szoveg: "true/false", helyes: true },
+          { szoveg: "Szám", helyes: false },
+          { szoveg: "String", helyes: false },
+          { szoveg: "Array", helyes: false },
+        ],
+      },
+      {
+        kerdesSzoveg: "Mi a System.out.println?",
+        valaszok: [
+          { szoveg: "Kiírás", helyes: true },
+          { szoveg: "Olvasás", helyes: false },
+          { szoveg: "Törlés", helyes: false },
+          { szoveg: "Mentés", helyes: false },
+        ],
+      },
+      {
+        kerdesSzoveg: "Mi a constructor?",
+        valaszok: [
+          { szoveg: "Inicializál", helyes: true },
+          { szoveg: "Töröl", helyes: false },
+          { szoveg: "Kiír", helyes: false },
+          { szoveg: "Ciklus", helyes: false },
         ],
       },
     ],
