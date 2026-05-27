db109512371cbb05d23708b056aa8e4798a853d0
molnarbea <molnarbeatrix11@gmail.com>
2026-05-27 15:12:14 +0200

    tesztek megirva

--- cypress/e2e/QuizCards.cy.js
+++ cypress/e2e/QuizCards.cy.js
@@ -1,5 +1,34 @@
 describe('template spec', () => {
   it('passes', () => {
     cy.visit('https://frnc36.github.io/QuizCards/')
-  })
+    cy.get('#kviz').trigger('mouseenter')
+    
+    cy.get('#kviz a').should('have.attr', 'href')
+    cy.get('#kviz a').click({ force: true })
+    cy.url().should('include', 'kvizTema.html')
+    
+  });
+
+  it('vissza a fooldalra', function() {
+    cy.visit('https://frnc36.github.io/QuizCards/kvizTema.html')
+    
+    cy.get('.home-link').click()
+    cy.url().should('include', 'index.html')
+    
+  });
+
+  it('tema kattintas', function() {
+    cy.visit('https://frnc36.github.io/QuizCards/kvizTema.html')
+    cy.get('#php').click()
+    cy.url().should('include', 'kviz.html')
+    
+    cy.visit('https://frnc36.github.io/QuizCards/kvizTema.html')
+    cy.get('#js').click()
+    cy.url().should('include', 'kviz.html')
+    
+    cy.visit('https://frnc36.github.io/QuizCards/kvizTema.html')
+    cy.get('#java').click()
+    cy.url().should('include', 'kviz.html')
+    
+  });
 })
