41f0330280d3b705f2bda73b41e92da78eec42ce
Frnc36 <magoriferenc19@gmail.com>
2026-06-01 19:16:51 +0200

    automatizalt doski proba

--- src/js/Kerdes.js
+++ src/js/Kerdes.js
@@ -1,9 +1,31 @@
 import Valasz from "./Valasz.js";
 
+/**
+ * @class Kerdes
+ * @classdesc Egy kvíz kérdést és a hozzá tartozó válaszokat kezelő osztály.
+ */
 export default class Kerdes {
+  /**
+   * @private
+   * @type {string}
+   * @description A kérdés szövege.
+   */
   #kerdesSzoveg;
+
+  /**
+   * @private
+   * @type {Array<Object>}
+   * @description A válaszokat tartalmazó lista.
+   */
   #valaszok = [];
 
+  /**
+   * @constructor
+   * @param {string} kerdesSzoveg - A kérdés szövege.
+   * @param {Array<Object>} valaszAdatok - A válaszok adatai.
+   * @param {HTMLElement} szuloElem - A szülő DOM elem.
+   * @param {Object} kviz - A kvíz vezérlő objektum (callback kezeléshez).
+   */
   constructor(kerdesSzoveg, valaszAdatok, szuloElem, kviz) {
     this.#kerdesSzoveg = kerdesSzoveg;
     this.#valaszok = valaszAdatok;
@@ -14,21 +36,35 @@ export default class Kerdes {
     this.megjelenit();
   }
 
-  /* Getter */
+  /**
+   * @method getKerdesSzoveg
+   * @returns {string}
+   * @description A kérdés szövegének visszaadása.
+   */
   getKerdesSzoveg() {
     return this.#kerdesSzoveg;
   }
 
+  /**
+   * @method getValaszok
+   * @returns {Array<Object>}
+   * @description A válaszlista visszaadása.
+   */
   getValaszok() {
     return this.#valaszok;
   }
 
-  /* Ezt kiegészíteném a válaszok számára egy divvel */
-  /* megfognám a valasz div-et 
+  /**
+   * @method megjelenit
+   * @description A kérdés és a hozzá tartozó válaszok megjelenítése.
+   * A válaszok példányosítása a Valasz osztállyal történik.
+   */
+  megjelenit() {
+    /* Ezt kiegészíteném a válaszok számára egy divvel */
+    /* megfognám a valasz div-et 
       /*  végigmegyek a listán és 
       példűányosítanám a kerdes osztáűlyt a lista elemeive
       a szülőelem a z utolsó valasz div. */
-  megjelenit() {
     let kod = `
           <div class="kerdes">
             <h2>${this.#kerdesSzoveg}</h2>
@@ -47,6 +83,12 @@ export default class Kerdes {
     });
   }
 
+  /**
+   * @private
+   * @method kever
+   * @param {Array} tomb - A válaszok tömbje.
+   * @description A válaszok sorrendjének véletlenszerű keverése.
+   */
   #kever(tomb) {
     for (let i = tomb.length - 1; i > 0; i--) {
       const j = Math.floor(Math.random() * (i + 1));
@@ -54,7 +96,12 @@ export default class Kerdes {
     }
   }
 
-  /* Saját */
+  /**
+   * @method helyesValaszE
+   * @param {Object} valasz - Egy válasz objektum.
+   * @returns {boolean}
+   * @description Megállapítja, hogy a válasz helyes-e.
+   */
   helyesValaszE(valasz) {
     return valasz.ishelyesE();
   }
